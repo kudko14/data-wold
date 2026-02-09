c7f1e951c490c513607ba6b3b893a0d3350a742a
Your Name <you@example.com>
2026-02-09 19:08:06 +0300

    bot

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+
+from random import choice
+import requests
+import asyncio
+from loguru import logger
+from dotenv import load_dotenv, find_dotenv
+from bs4 import BeautifulSoup
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+
+load_dotenv(find_dotenv())
+TOKEN = os.getenv("TOKEN")
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def command_start_handler(message: Message):
+    logger.info(message.from_user.id)
+    logger.info(message.from_user.first_name)
+    logger.info(message.from_user.last_name)
+    logger.info(message.from_user.username)
+    logger.info(message.from_user.language_code)
+
+    await message.answer("Привет! Я рассказываю анекдоты!\nИспользуй:\n/start\n/anekdot")
+
+@dp.message(Command("anekdot"))
+async def send_anekdot(message: Message):
+    response = requests.get('http://www.anekdot.ru/random/anekdot/')
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        jokes = soup.find_all('div', class_='text')
+
+        random_joke = choice(jokes).text.strip()
+        anekdot = random_joke
+    else:
+        anekdot = "Не удалось получить анекдот"
+
+    await message.answer(anekdot)
+
+
+async def main():
+    logger.add('file.log',
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation="3 days")
+
+    bot = Bot(token=TOKEN)
+    logger.info("Бот создан... ")
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main()) 
